3ad32a6e1e679d1db85a9b1bd0ec1e28640a9677
Bedwey <bedweyy@gmail.com>
2023-03-21 05:23:44 -0700

    C - Functions, nested loops 🚀

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description: print value of n status; greater than 5,
+* is zero or less than 6 and not equal 0
+*
+* Return: 0 (End Program)
+*/
+
+int main(void)
+{
+	int start = 0;
+	char str[] = "_putchar";
+
+	while (start < sizeof(str))
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
