b70800355de2c635b2b4f49492f6304f27f7ac70
Bedwey <bedweyy@gmail.com>
2023-04-03 14:53:05 -0700

    anthor test

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,14 +13,12 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 		s++;
 	}
-
 	if (*s == c)
-		return (0);
-
+	{
+		return (s);
+	}
 	return (0);
 }
