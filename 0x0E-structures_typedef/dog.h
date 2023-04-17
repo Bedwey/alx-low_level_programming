55ee3906ebfab2a6913e1eb0781a2fb1c727da86
Bedwey <bedweyy@gmail.com>
2023-04-17 13:08:00 -0700

    Poppy woof! 🐶

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog info
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ *
+ * Description: dog struct to store info in it.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
