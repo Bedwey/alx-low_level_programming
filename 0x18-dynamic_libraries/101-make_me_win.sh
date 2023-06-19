6807ba80246f1b9cfebd4f4c3f67e52e85747bb7
Bedwey <bedweyy@gmail.com>
2023-06-19 11:28:54 -0700

    fix repo

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Bedwey/alx-low_level_programming/raw/main/0x18-dynamic_libraries/ihack.so
+wget -P /tmp https://github.com/Bedwey/alx-low_level_programming/raw/master/0x18-dynamic_libraries/ihack.so
 export LD_PRELOAD=/tmp/ihack.so
